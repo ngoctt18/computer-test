7ea29490192ee885242bc0b8d0018dbb156a5e3d
trantuanngoctn97 <trantuanngoctn97@gmail.com>
2020-01-16 10:50:16 +0700

    update favicon

--- resources/views/website/partials/head.blade.php
+++ resources/views/website/partials/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Favicon -->
-<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.jpg') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
 
 <!-- all css here -->
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
