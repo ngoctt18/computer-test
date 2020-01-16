cdb8bfc2b65f360c8ad2d4a36c46dfe92622d426
trantuanngoctn97 <trantuanngoctn97@gmail.com>
2020-01-16 11:32:54 +0700

    DashboardController add raw 2

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -24,11 +24,10 @@ class DashboardController extends Controller
         $productCount = Product::all()->count();
         $orders = Order::all();
         $statistics = DB::table('orders')->sum('total_money');
-        $sum_statistics = DB::select(DB::raw(
- 'SELECT YEAR(`created_at`) as year, SUM(`total_money`) AS total FROM `orders` GROUP BY YEAR(`created_at`)'));
+        $sum_statistics = DB::select(DB::raw("SELECT YEAR('created_at') as year, SUM('total_money') AS total FROM 'orders' GROUP BY YEAR('created_at')"));
         // dd($sum_statistics);
-        $bestseller = DB::select(DB::raw('SELECT `order_details`.`product_id` as id, name,  SUM(`order_details`.`quantity`) as quantity, COUNT(`order_details`.`order_id`) as numberord FROM `order_details`, `products` WHERE `order_details`.`product_id` = `products`.`id` GROUP BY `product_id`, `name` ORDER BY quantity DESC LIMIT 10'));
-        $oldproducts = DB::select(DB::raw('SELECT `id`,`code`, `name`, `quantity`, `price`, `created_at` FROM `products` WHERE YEAR(`created_at`) <= (year(CURRENT_DATE) - 1) AND `quantity`>= 50'));
+        $bestseller = DB::select(DB::raw("SELECT 'order_details'.'product_id' as id, name,  SUM('order_details'.'quantity') as quantity, COUNT('order_details'.'order_id') as numberord FROM 'order_details', 'products' WHERE 'order_details'.'product_id' = 'products'.'id' GROUP BY 'product_id', 'name' ORDER BY quantity DESC LIMIT 10"));
+        $oldproducts = DB::select(DB::raw("SELECT 'id','code', 'name', 'quantity', 'price', 'created_at' FROM 'products' WHERE YEAR('created_at') <= (year(CURRENT_DATE) - 1) AND 'quantity'>= 50"));
         //  dd($oldproducts);
         return view('admin.layouts.index', compact('userCountCus', 'userCountEm', 'orderCount', 'productCount', 'orders', 'bestseller', 'oldproducts', 'statistics', 'sum_statistics'));
     }
